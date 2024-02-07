30aee35f6381a27516eb4a5e87690326bc83a874
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-02-07 17:31:32 +0530

    Create 451. Sort Characters By Frequency.cpp

---
+++ 451. Sort Characters By Frequency.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        for(char it : s) mp[it]++;
+        auto lamda = [&](char &a,char &b){
+            // return mp[a] > mp[b] || (mp[a] == mp[b] && a < b);
+            if (mp[a] != mp[b]) {
+                return mp[a] > mp[b];
+            }
+            return a < b;
+        };
+        sort(s.begin(),s.end(),lamda);
+        return s;
+    }
+};
