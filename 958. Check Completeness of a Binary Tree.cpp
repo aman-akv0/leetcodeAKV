6649adecec1362462f8ec90d2cf3554c5c625f56
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-02-08 23:23:38 +0530

    Update 958. Check Completeness of a Binary Tree.cpp

--- 958. Check Completeness of a Binary Tree.cpp
+++ 958. Check Completeness of a Binary Tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+DFS
+
 class Solution {
 public:
     int countNodes(TreeNode* root){
@@ -25,5 +27,30 @@ public:
         int i=1;
         return dfs(root,i,count);
         
+    }
+};
+
+or
+
+BFS
+class Solution {
+public:
+    
+    bool isCompleteTree(TreeNode* root) {
+        vector<int> ans;
+        queue<TreeNode*> que;
+        que.push(root);
+        bool pre=false;
+        TreeNode* t = NULL;
+        while(!que.empty()){
+            t=que.front();
+            que.pop();
+            if(t==NULL) pre=true;
+            else{
+                if(pre==true) return false;
+                que.push(t->left);
+                que.push(t->right); 
+            }
+        }return true;
     }
 };
