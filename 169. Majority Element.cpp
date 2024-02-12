a408e7bce3961e505caae54804f908291865c3a8
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-02-12 21:21:54 +0530

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -35,3 +35,14 @@ public:
         }return 0;
     }
 };
+
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[n/2];
+    }
+};
