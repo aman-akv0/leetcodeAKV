b2e94b598708cd1e02cd327a0b06d60b4346302c
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-02-12 20:54:39 +0530

    Create 169. Majority Element.cpp

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int count = 0;
+//         int candidate = 0; 
+        
+//         for (int i = 0; i < n; i++) {
+//             if (count == 0) {
+//                 candidate = nums[i];
+//                 count = 1;
+//             } else {
+//                 if (nums[i] == candidate) {
+//                     count++;
+//                 } else {
+//                     count--;
+//                 }
+//             }
+//         }
+        
+             
+//         return candidate;
+//     }
+// };
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int , int>mp;
+        for(auto it:nums) mp[it]++;
+        for(auto i : mp){
+            if(i.second>(n/2)) return i.first;
+        }return 0;
+    }
+};
