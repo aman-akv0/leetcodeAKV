a42be5e8750db16c776709ef0dcd3d7e22ea3113
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-03-04 20:51:28 +0530

    Create 948. Bag of Tokens.cpp

---
+++ 948. Bag of Tokens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+    int bagOfTokensScore(vector<int>& tokens, int P)
+    {
+        sort(tokens.begin(), tokens.end());
+        int result = 0, points = 0;
+        int left = 0, right = tokens.size() - 1;
+        while (left <= right) 
+        {
+            if (P >= tokens[left]) 
+            {
+                P -= tokens[left];
+                left++;
+                points++;
+                result = max(result, points);
+            } 
+            else if (points > 0) 
+            {
+                --points;
+                P += tokens[right];
+                right--;
+            } 
+            else 
+            {
+                break;
+            }
+        }
+        return result;
+    }
+};
