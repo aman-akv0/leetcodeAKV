2ce6c558e23dea1be5a6f7de257108ac349afa52
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-03-14 22:05:13 +0530

    Create 930. Binary Subarrays With Sum.cpp

---
+++ 930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        unordered_map<int , int> mp;
+        mp[0]=1;
+        int n=nums.size();
+        int res=0;
+        int pre=0;
+        for(int i=0; i<n;i++){
+            pre+=nums[i];
+            int neww=pre-k;
+            if(mp.find(neww)==mp.end()){
+                mp[pre]++;
+            }
+            else{
+                res+=mp[neww];
+                mp[pre]++;
+            }
+            
+            
+
+        }return res;
+    }
+};
